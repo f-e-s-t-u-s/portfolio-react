71344383e386eddf56ff7de9b7dd56cdf25948d0
Festus <festusgitahik@gmail.com>
2023-06-11 20:55:08 +0300

    Made component one

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css"
+import Spotlight from "./One";
+
+function Home(){
+    return(
+        <div className="home">
+            <div className="one">
+        <Spotlight></Spotlight>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
