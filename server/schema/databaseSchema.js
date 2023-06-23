69c63aa9a2a8ae35ae7fa23d510757178cd58bc0
Festus <festusgitahik@gmail.com>
2023-06-23 09:42:15 +0300

    Created database schema

---
+++ server/schema/databaseSchema.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+const authorSchema = new mongoose.Schema({
+    first_name : String,
+    last_name : String,
+    email : String,
+    passwordHash : String,
+    profile : String,
+});
+
+const postSchema = new mongoose.Schema({
+    // author id
+    title : String,
+    summary : String,
+    content : String,
+    comment : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Comment'
+    },
+    postCategory : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'PostCategory'
+    },
+
+
+})
+
+const post_commentSchema = new mongoose.Schema({
+    title: String,
+    content : String,
+
+
+})
+
+const categorySchema = new mongoose.Schema({
+    title: String,
+    content : String,
+})
+
+const post_categorySchema = new mongoose.Schema({
+   Author: {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'Author'
+   },
+   tag : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'Tag'
+   },
+   category : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'Category'
+   }
+})
+
+const tagSchema = new mongoose.Schema({
+    content : String,
+
+})
+
+
+const Author = mongoose.model('Author', authorSchema);
+const Post = mongoose.model('Post', postSchema)
+const Comment = mongoose.model('Comment', post_commentSchema)
+const Category = mongoose.model('Category', categorySchema)
+const PostCategory = mongoose.model('PostCategory', post_categorySchema)
+const Tag = mongoose.model('Tag', tagSchema)
